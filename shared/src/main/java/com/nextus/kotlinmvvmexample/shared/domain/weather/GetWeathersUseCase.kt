6f03e72c2f6ed9c044a8a3d230032bf5948b175c
chosw1029 <chosw10293@naver.com>
2020-09-08 17:35:34 +0900

    Flow 수정

--- shared/src/main/java/com/nextus/kotlinmvvmexample/shared/domain/weather/GetWeathersUseCase.kt
+++ shared/src/main/java/com/nextus/kotlinmvvmexample/shared/domain/weather/GetWeathersUseCase.kt
@@ -3,17 +3,29 @@ package com.nextus.kotlinmvvmexample.shared.domain.weather
 import com.nextus.kotlinmvvm.model.Weather
 import com.nextus.kotlinmvvmexample.shared.data.weather.WeatherRepository
 import com.nextus.kotlinmvvmexample.shared.di.IoDispatcher
+import com.nextus.kotlinmvvmexample.shared.domain.FlowUseCase
 import com.nextus.kotlinmvvmexample.shared.domain.UseCase
+import com.nextus.kotlinmvvmexample.shared.result.Result
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 open class GetWeathersUseCase @Inject constructor(
     private val weatherRepository: WeatherRepository,
     @IoDispatcher dispatcher: CoroutineDispatcher
-): UseCase<Unit, List<Weather>>(dispatcher) {
+): FlowUseCase<Unit, List<Weather>>(dispatcher) {
 
-    override suspend fun execute(parameters: Unit): List<Weather> {
-        return weatherRepository.getWeathers()
+    override fun execute(parameters: Unit): Flow<Result<List<Weather>>> {
+        return weatherRepository.getWeathers().map { result ->
+            when(result) {
+                is Result.Success -> {
+                    Result.Success(result.data)
+                }
+                is Result.Loading -> result
+                is Result.Error -> result
+            }
+        }
     }
 
 }
